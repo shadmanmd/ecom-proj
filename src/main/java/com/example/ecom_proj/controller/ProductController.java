542d952409a94a4d9ba76bd94915f7b91dd4f019
Md Shadman <44136375+shadmanmd@users.noreply.github.com>
2026-04-27 06:43:55 +0530

    Model created and tested on h2-console

--- src/main/java/com/example/ecom_proj/controller/ProductController.java
+++ src/main/java/com/example/ecom_proj/controller/ProductController.java
@@ -1,7 +1,15 @@
 package com.example.ecom_proj.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+
 }
---
+++ src/main/java/com/example/ecom_proj/model/Product.java
@@ -0,0 +1,30 @@
+package com.example.ecom_proj.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+
+}
